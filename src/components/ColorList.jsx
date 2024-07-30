ede67a976422236cc4eb380afc5fa381b943aa45
simanto-sroy <sroy38703@gmail.com>
2024-07-30 23:06:21 +0600

    context-consumner

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -4,17 +4,20 @@ import { ColorContext } from '../main';
 import Color from './Color';
 
 export default function ColorList({onRemoveColor = f => f, onRateColor = f => f}) {
-    
-    const {colors} = useContext(ColorContext)
-    console.log(colors)
-
-    if(!colors.length) return <div>No Colors Listed. (Add a Color)</div>
-
     return (
-        <div className="color-list">
-            {
-                colors.map(color => <Color key={color.id} {...color} />)
-            }
-        </div>
+        <ColorContext.Consumer>
+            {context => {
+                if(!context.colors.length) {
+                    return <>No Colors Listed. (Add a Color)</>
+                }
+                return (
+                    <>
+                        {context.colors.map(color => (
+                            <Color key={color.id} {...color} />
+                        ))}
+                    </>
+                )
+            }}
+        </ColorContext.Consumer>
     )
 }
